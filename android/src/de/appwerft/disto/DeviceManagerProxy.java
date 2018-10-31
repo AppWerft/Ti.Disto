1166a9cac4d310f52127bc1d9bc45285a737f4b6
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2018-10-31 13:27:20 +0100

    debug

--- android/src/de/appwerft/disto/DeviceManagerProxy.java
+++ android/src/de/appwerft/disto/DeviceManagerProxy.java
@@ -31,16 +31,12 @@ public class DeviceManagerProxy extends KrollProxy implements
 		ErrorListener {
 	private Device currentDevice;
 	private Context ctx;
- //   static TestContext testContext;
-	private DeviceManager deviceManager;
-	// needed for connection timeout
+ 	private DeviceManager deviceManager;
 	private Timer connectionTimeoutTimer;
 	private TimerTask connectionTimeoutTask;
-	// to do infinite rounds of finding devices
 	private Timer findDevicesTimer;
 	boolean findDevicesRunning = false;
 	boolean activityStopped = true;
-	// to handle user cancel connection attempt
 	private Map<Device, Boolean> connectionAttempts = new HashMap<>();
 	private Device currentConnectionAttemptToDevice = null;
 	public static boolean DEBUG = false;
@@ -48,42 +44,19 @@ public class DeviceManagerProxy extends KrollProxy implements
 	private Activity activity;
 	public static final String LCAT = TidistoModule.LCAT;
 
-	/**
-	 * Defines the default behavior when an error is notified. Presents alert to
-	 * user showsing a error message
-	 *
-	 * @param errorObject
-	 *            error object comes from different sources SDK or APP.
-	 */
 	@Override
 	public void onError(ErrorObject err, Device device) {
 		Log.e(LCAT, err.getErrorMessage());
-
 	}
 
-	/**
-	 * Called when the connection state of a device changed
-	 *
-	 * @param device
-	 *            currently connected device
-	 * @param state
-	 *            current device connection state
-	 */
 	@Override
 	public void onConnectionStateChanged(final Device device,
 			ConnectionState state) {
 		Log.i(LCAT, device.getModel() + "  " + state);
 	}
 
-	/**
-	 * called when a valid Leica device is found
-	 *
-	 * @param device
-	 *            the device
-	 */
 	@Override
 	public void onAvailableDeviceFound(final Device device) {
-		Log.i(LCAT, "Hurra! |||||||||||||||||||||||||||||||");
 		Log.i(LCAT,
 				"Model: " + device.getModel() + " Name: "
 						+ device.getDeviceName());
@@ -108,9 +81,7 @@ public class DeviceManagerProxy extends KrollProxy implements
 
 	public DeviceManagerProxy() {
 		super();
-		
 	}
-	
 	
 	@Override
 	public void handleCreationDict(
