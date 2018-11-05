fa6a127a22dd97f868e6324da59aedc3957ba409
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2018-11-05 10:06:14 +0100

    README_SMS.md

--- LeicaSDK/disto_source_from_jdcore/ch/leica/sdk/BuildConfig.java
+++ LeicaSDK/disto_source_from_jdcore/ch/leica/sdk/BuildConfig.java
@@ -1,13 +1,13 @@
 package ch.leica.sdk;
 
-public final class BuildConfig
-{
-  public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "ch.leica.sdk";
-  public static final String BUILD_TYPE = "release";
-  public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 102;
-  public static final String VERSION_NAME = "1.0.0.2";
-  
-  public BuildConfig() {}
+public final class BuildConfig {
+	public static final boolean DEBUG = false;
+	public static final String APPLICATION_ID = "ch.leica.sdk";
+	public static final String BUILD_TYPE = "release";
+	public static final String FLAVOR = "";
+	public static final int VERSION_CODE = 102;
+	public static final String VERSION_NAME = "1.0.0.2";
+
+	public BuildConfig() {
+	}
 }
