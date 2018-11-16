eab1a95617990b5a27d6d7d02b4b8a10ede69612
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2018-11-16 13:03:05 +0100

    README

--- android/src/de/appwerft/disto/CommandsDialog.java
+++ android/src/de/appwerft/disto/CommandsDialog.java
@@ -8,6 +8,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.widget.EditText;
 import ch.leica.sdk.Types;
 import ch.leica.sdk.Devices.Device;
 import ch.leica.sdk.ErrorHandling.DeviceException;
@@ -26,7 +27,7 @@ public class CommandsDialog {
 	private AlertDialog customCommandDialog;
 	private AlertDialog commandDialog;
 	private static String LCAT = TidistoModule.LCAT;
-
+	AlertDialog.Builder alertConnectedBuilder = new AlertDialog.Builder(TiApplication.getAppCurrentActivity());
 	
 	public CommandsDialog() {
 	}
@@ -51,7 +52,7 @@ public class CommandsDialog {
 								.getAvailableCommands()[which];
 
 						if (command.equals(Types.Commands.Custom.name())) {
-							// showCustomCommandDialog();
+							showCustomCommandDialog();
 						} else {
 							commandDialog.hide();
 							new Thread(new Runnable() {
@@ -95,5 +96,73 @@ public class CommandsDialog {
 
 		
 	}
+	
+	void showCustomCommandDialog() {
+
+
+		final String METHODTAG = ".showCustomCommandDialog";
+		AlertDialog.Builder customCommandDialogBuilder = new AlertDialog.Builder(TiApplication.getAppCurrentActivity());
+		final EditText input = new EditText(TiApplication.getAppCurrentActivity());
+		customCommandDialogBuilder.setTitle("Custom Command");
+		customCommandDialogBuilder.setView(input);
+		customCommandDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+
+				if (sendCustomCommandThread == null) {
+					sendCustomCommandThread =
+							new HandlerThread(
+									"getDeviceStateThread" + System.currentTimeMillis(),
+									HandlerThread.MAX_PRIORITY
+							);
+
+					sendCustomCommandThread.start();
+					sendCustomCommandHandler = new Handler(sendCustomCommandThread.getLooper());
+				}
+
+				// send any string to device
+				try {
+					sendCustomCommandHandler.post(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								Response response =
+										currentDevice.sendCustomCommand(
+												input.getText().toString()/*,
+												currentDevice.getTIMEOUT_NORMAL()*/
+										);
+
+								response.waitForData();
+
+								if (response.getError() != null) {
+									Log.e(LCAT,
+											METHODTAG + ": error: " + response.getError().getErrorMessage()
+									);
+								}
+//								setDistoComResponse(((ResponsePlain) response).getReceivedDataString());
+								Log.d(LCAT, METHODTAG + "DistoComResponse set with ResponsePlain");
+
+							} catch (DeviceException e) {
+
+								Log.e(LCAT, METHODTAG + ": Error sending the command.", e);
+							}
+						}
+					});
+
+				} catch (Exception e) {
+					Log.e(LCAT, METHODTAG + ": Error showCustomCommandDialog ", e);
+				}
+			}
+		});
+		customCommandDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		customCommandDialog = customCommandDialogBuilder.create();
+		customCommandDialog.show();
+}
 
 }
